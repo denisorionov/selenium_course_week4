8ba619c80742292908f5c9da2ce43769941f61b0
Denis Orionov <orionovdv@gmail.com>
2021-06-05 11:07:13 +0300

    Add tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,35 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
-
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_should_see_login_url(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page = page.go_to_login_page()
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_form(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_should_see_register_form(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
