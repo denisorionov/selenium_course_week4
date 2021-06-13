301e9575dd9e23d2f6b2ad87a7eda4f9cbdd32fa
Denis Orionov <orionovdv@gmail.com>
2021-06-13 09:08:15 +0300

    Add promo offer links and xfail for 7 offer

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,16 @@
 import time
 
+import pytest
+
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('promo_offer',
+                         [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason='')) for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    time.sleep(2)
+    time.sleep(1)
     page.should_be_book_name()
