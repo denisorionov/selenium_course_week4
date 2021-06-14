03fb7be24ef081ab5f4161ab219ce432f78aa3b1
Denis Orionov <orionovdv@gmail.com>
2021-06-14 13:04:51 +0300

    Remove common def to base_page.py and add tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,16 +1,18 @@
 import math
-from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.common.exceptions import NoAlertPresentException, TimeoutException
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+from .locators import BasePageLocators
 
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        #self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -52,3 +54,10 @@ class BasePage:
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
